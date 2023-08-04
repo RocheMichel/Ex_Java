703b1cdf583485c47cac0584f9e8c086bebcd909
Roche <michel.roche@le-campus-numerique.fr>
2023-08-04 09:08:32 +0200

    initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int nbr_de_carton_dans_lancien_local = 32;
+        int nbr_de_carton_dans_nouveau_local = 0;
+        int capacite_du_chargement_du_camion = 9;
+        int _le_camion = 0;
+        int je_prend_un_carton_je_le_met_dans_le_camion = 1;
+        int jenleve_un_carton_je_le_met_dans_le_nouveau_local = 1;
+        int i =0;
+
+
+
+        }
+
+    }
+}
